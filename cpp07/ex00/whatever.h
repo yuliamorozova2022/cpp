5e3148aa9d4c137ba4bc3a21de319f7fe98676c2
Yulia Morozova <yuliamorozova@Yulias-MacBook-Pro.local>
2024-02-02 11:42:58 +0100

    fix cpp07/ex00

--- cpp07/ex00/whatever.h
+++ cpp07/ex00/whatever.h
@@ -3,9 +3,8 @@
 
 #include <iostream>
 
-template <typename T> void swap(T a, T b) {
-    T tmp;
-    tmp = a;
+template <typename T> void swap(T &a, T &b) {
+    T tmp = a;
     a = b;
     b = tmp;
 }
